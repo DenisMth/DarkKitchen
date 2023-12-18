aa81c7ebf388f7757a979d2d84f5ec028874843c
Denis Mathieu <denismathieu1203@gmail.com>
2023-12-18 14:50:38 +0100

    Denis added mainCourse array

---
+++ assets/mainCourse.js
@@ -0,0 +1,86 @@
+const food_mainCourse = [
+    {
+        name : "Vol-au-vent",
+        ingredients : ["Poulet", "Champignons", "Riz", "Pâte feuilletée"],
+        image : "assets/resources/images/Vol-au-vent.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Steak frites",
+        ingredients : ["Boeuf","Pommes de terre","Huile d'olive", "salade"],
+        image : "assets/resources/images/Steak_frites.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Hachis parmentier",
+        ingredients : ["Gruyère", "Oignons", "Tomates", "Pommes de terre", "lait", "Porc", "Boeuf", "Oeufs", "Ail"],
+        image : "assets/resources/images/Hachis_parmentier.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Salade de topinambour",
+        ingredients : ["Quinoa", "Chou rouge", "carottes", "poire", "raisins secs", "huile d'olive", "vinaigre de cidre", "mayonnaise"],
+        image : "assets/resources/images/Salade_topinambour.jpg",
+        vegetarien : true,
+    },
+
+    {
+        name : "Pâtes aux champignons et miso",
+        ingredients : ["Pâtes", "Huile d'olive", "Champignons de Paris", "Ail", "Échalottes", "Crème fraîche", "Miso blanc", "Parmesan", "Algues nori"],
+        image : "assets/resources/images/Pates_champignons_miso.jpg",
+        vegetarien : true,
+    },
+
+    {
+        name : "Lasagne",
+        ingredients : ["Lasagnes","Muscade", "Gruyère", "Tomates", "Vin rouge", "Boeuf", "Carottes", "Oignons jaunes", "Parmesan", "Lait", "Basilic", "Céleri", "Ail", "Laurier", "Thym"],
+        image : "assets/resources/images/Lasagne.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Lasagne végétarienne",
+        ingredients : ["Oignons", "Gruyère", "Lasagnes", "Courgettes", "Tomates", "Basilic", "Herbes de provence", "Crème fraîche", "Céleri", "Ail", "Thym"],
+        image : "assets/resources/images/Lasagne_vegetarienne.jpg",
+        vegetarien : true,
+    },
+
+    {
+        name : "Poivrons farcis à la grecque",
+        ingredients : ["Tomates", "Riz", "Feta", "Oignons", "Pignons de pin", "Raisins secs", "Huile d'olive", "Curcuma", "Thym"],
+        image : "assets/resources/images/Poivrons_farcis_grecs.jpg",
+        vegetarien : true,
+    },
+
+    {
+        name : "Mijoté de tofu aux champignons",
+        ingredients : ["Tofu", "Champignons", "Huile d'olive", "Haricots", "Pois", "Riz", "Carottes"],
+        image : "assets/resources/images/Tofu_aux_champignons.jpg",
+        vegetarien : true,
+    },
+
+    {
+        name : "Pâtes carbonara",
+        ingredients : ["Spaghettis", "Oeufs", "Parmesan", "Lardons", "Huile d'olive", "Crème fraîche"],
+        image : "assets/resources/images/Pates_carbonara.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Tartiflette",
+        ingredients : ["Huile d'olive", "Reblochon", "Ail", "Lardons fumés", "Pommes de terre", "Oignons", "Persil"],
+        image : "assets/resources/images/Tartiflette.jpg",
+        vegetarien : false,
+    },
+
+    {
+        name : "Boulettes sauce tomate",
+        ingredients : ["Tomates", "Boeuf", "Persil", "Pommes de terre", "Oignons", "Huile d'olive", "Ail"],
+        image : "assets/resources/images/Boulettes_sauce_tomate.jpg",
+        vegetarien : false,
+    },
+];
+
