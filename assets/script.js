546e781ee3cc8b97feda53568b5999bdd2cda7f4
Newoz <josh.zaninello@gmail.com>
2023-12-20 10:50:48 +0100

    essai panier

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+
+
+
 let food_entrees = [
     {
         name: "Bruschetta",
@@ -86,193 +89,192 @@ let food_entrees = [
 ];
 
 
-let  food_desserts =[
+let food_desserts = [
     {
         name: "Chocolate Cake",
         ingredients: ["butter", "vanilla extract", "sugar", "chocolate", "milk", "eggs"],
         vegetarian: true,
         price: "5.20",
         picture: 'assets/resources/images/chocolate_cake.jpg'
-      },
-      {
+    },
+    {
         name: "Cheesecake",
         ingredients: ["cream cheese", "sugar", "eggs", "vanilla extract", "butter", "graham cracker crust"],
         vegetarian: true,
         price: "6.50",
         picture: 'assets/resources/images/cheesecake.jpg'
-      },
-      {
+    },
+    {
         name: "Apple Pie",
         ingredients: ["apples", "sugar", "cinnamon", "nutmeg", "butter", "pie crust"],
         vegetarian: true,
         price: "4.75",
         picture: 'assets/resources/images/apple_pie.jpg'
-      },
-      {
+    },
+    {
         name: "Tiramisu",
         ingredients: ["ladyfingers", "espresso", "mascarpone cheese", "cocoa powder", "eggs", "sugar"],
         vegetarian: true,
         price: "7.00",
         picture: 'assets/resources/images/tiramisu.jpg'
-      },
-      {
+    },
+    {
         name: "Ice Cream Sundae",
         ingredients: ["vanilla ice cream", "chocolate syrup", "whipped cream", "nuts", "cherry"],
         vegetarian: true,
         price: "4.50",
         picture: 'assets/resources/images/ice_cream_sundae.jpg'
-      },
-      {
+    },
+    {
         name: "Pecan Pie",
         ingredients: ["pecans", "corn syrup", "eggs", "sugar", "butter", "pie crust"],
         vegetarian: true,
         price: "6.25",
         picture: 'assets/resources/images/pecan_pie.jpg'
-      },
-      {
+    },
+    {
         name: "Strawberry Shortcake",
         ingredients: ["strawberries", "whipped cream", "shortcake biscuits"],
         vegetarian: true,
         price: "5.75",
         picture: 'assets/resources/images/strawberry_shortcake.jpg'
-      },
-      {
+    },
+    {
         name: "Mousse au Chocolat",
         ingredients: ["dark chocolate", "whipping cream", "sugar", "egg yolks"],
         vegetarian: true,
         price: "8.00",
         picture: 'assets/resources/images/mousse_au_chocolat.jpg'
-      },
-      {
+    },
+    {
         name: "Red Velvet Cupcakes",
         ingredients: ["flour", "cocoa powder", "buttermilk", "cream cheese frosting"],
         vegetarian: true,
         price: "3.95",
         picture: 'assets/resources/images/red_velvet_cupcakes.jpg'
-      },
-      {
+    },
+    {
         name: "Lemon Sorbet",
         ingredients: ["lemons", "sugar", "water"],
         vegetarian: true,
         price: "4.25",
         picture: 'assets/resources/images/lemon_sorbet.jpg'
-      },
-      {
+    },
+    {
         name: "Cannoli",
         ingredients: ["ricotta cheese", "powdered sugar", "cinnamon", "pistachios", "cannoli shells"],
         vegetarian: true,
         price: "6.75",
         picture: 'assets/resources/images/cannoli.jpg'
-      },
-      {
+    },
+    {
         name: "Banana Split",
         ingredients: ["banana", "vanilla ice cream", "chocolate syrup", "strawberry topping", "whipped cream", "nuts", "cherry"],
         vegetarian: true,
         price: "7.50",
         picture: 'assets/resources/images/banana_split.jpg'
-      }
-    ]; 
+    }
+];
 
 
-    let food_mainCourse = [
-        {
-            name : "Vol-au-vent",
-            ingredients : ["Chicken", "Mushrooms", "Rice", "Pâte feuilletée"],
-            picture : "assets/resources/images/Vol-au-vent.jpg",
-            vegetarian : false,
-            price : 8.95,
-        },
-    
-        {
-            name : "Steak frites",
-            ingredients : ["Beef","Potatoes","Olive oil", "Lettuce"],
-            picture : "assets/resources/images/Steak_frites.jpg",
-            vegetarian : false,
-            price : 9.64,
-        },
-    
-        {
-            name : "Hachis parmentier",
-            ingredients : ["Gruyère", "Oignons", "Tomatoes", "Potatoes", "Milk", "Pork", "Beef", "Eggs", "Ail"],
-            picture : "assets/resources/images/Hachis_parmentier.jpg",
-            vegetarian : false,
-            price : 8.42,
-        },
-    
-        {
-            name : "Salade de topinambour",
-            ingredients : ["Quinoa", "Red cabbage", "Carrots", "Pears", "Dried raisins", "Olive oil", "CIder vinegar", "Mayonnaise"],
-            picture : "assets/resources/images/Salade_topinambour.jpg",
-            vegetarian : true,
-            price : 11.53,
-        },
-    
-        {
-            name : "Pâtes aux champignons et miso",
-            ingredients : ["Pasta", "Olive oil", "Paris Mushrooms", "Ail", "Échalottes", "Fresh cream", "Miso blanc", "Parmesan", "Algues nori"],
-            picture : "assets/resources/images/Pates_champignons_miso.jpg",
-            vegetarian : true,
-            price : 12.46,
-        },
-    
-        {
-            name : "Lasagne",
-            ingredients : ["Lasagnes","Muscade", "Gruyère", "Tomatoes", "Red wine", "Beef", "Carrots", "Oignons jaunes", "Parmesan", "Milk", "Basilic", "Céleri", "Ail", "Laurier", "Thym"],
-            picture : "assets/resources/images/Lasagne.jpg",
-            vegetarian : false,
-            price : 9.78,
-        },
-    
-        {
-            name : "Lasagne végétarienne",
-            ingredients : ["Oignons", "Gruyère", "Lasagnes", "Courgettes", "Tomatoes", "Basilic", "Herbes de provence", "Fresh cream", "Céleri", "Ail", "Thym"],
-            picture : "assets/resources/images/Lasagne_vegetarienne.jpg",
-            vegetarian : true,
-            price : 11.45,
-        },
-    
-        {
-            name : "Poivrons farcis à la grecque",
-            ingredients : ["Tomatoes", "Rice", "Feta", "Oignons", "Pignons de pin", "Dried raisins", "Olive oil", "Curcuma", "Thym"],
-            picture : "assets/resources/images/Poivrons_farcis_grecs.jpg",
-            vegetarian : true,
-            price : 9.98,
-        },
-    
-        {
-            name : "Mijoté de tofu aux champignons",
-            ingredients : ["Tofu", "Mushrooms", "Olive oil", "Haricots", "Peas", "Rice", "Carrots"],
-            picture : "assets/resources/images/Tofu_aux_champignons.jpg",
-            vegetarian : true,
-            price : 12.56,
-        },
-    
-        {
-            name : "Pâtes carbonara",
-            ingredients : ["Spaghettis", "Eggs", "Parmesan", "Lardons", "Olive oil", "Fresh cream"],
-            picture : "assets/resources/images/Pates_carbonara.jpg",
-            vegetarian : false,
-            price : 8.57,
-        },
-    
-        {
-            name : "Tartiflette",
-            ingredients : ["Olive oil", "Reblochon", "Ail", "Lardons fumés", "Potatoes", "Oignons", "Persil"],
-            picture : "assets/resources/images/Tartiflette.jpg",
-            vegetarian : false,
-            price : 10.87,
-        },
-    
-        {
-            name : "Boulettes sauce tomate",
-            ingredients : ["Tomatoes", "Beef", "Persil", "Potatoes", "Oignons", "Olive oil", "Ail"],
-            picture : "assets/resources/images/Boulettes_sauce_tomate.jpg",
-            vegetarian : false,
-            price : 8.23,
-        },
-    ];
+let food_mainCourse = [
+    {
+        name: "Vol-au-vent",
+        ingredients: ["Chicken", "Mushrooms", "Rice", "Pâte feuilletée"],
+        picture: "assets/resources/images/Vol-au-vent.jpg",
+        vegetarian: false,
+        price: 8.95,
+    },
 
-    console.log(food_entrees);
+    {
+        name: "Steak frites",
+        ingredients: ["Beef", "Potatoes", "Olive oil", "Lettuce"],
+        picture: "assets/resources/images/Steak_frites.jpg",
+        vegetarian: false,
+        price: 9.64,
+    },
+
+    {
+        name: "Hachis parmentier",
+        ingredients: ["Gruyère", "Oignons", "Tomatoes", "Potatoes", "Milk", "Pork", "Beef", "Eggs", "Ail"],
+        picture: "assets/resources/images/Hachis_parmentier.jpg",
+        vegetarian: false,
+        price: 8.42,
+    },
+
+    {
+        name: "Salade de topinambour",
+        ingredients: ["Quinoa", "Red cabbage", "Carrots", "Pears", "Dried raisins", "Olive oil", "CIder vinegar", "Mayonnaise"],
+        picture: "assets/resources/images/Salade_topinambour.jpg",
+        vegetarian: true,
+        price: 11.53,
+    },
+
+    {
+        name: "Pâtes aux champignons et miso",
+        ingredients: ["Pasta", "Olive oil", "Paris Mushrooms", "Ail", "Échalottes", "Fresh cream", "Miso blanc", "Parmesan", "Algues nori"],
+        picture: "assets/resources/images/Pates_champignons_miso.jpg",
+        vegetarian: true,
+        price: 12.46,
+    },
+
+    {
+        name: "Lasagne",
+        ingredients: ["Lasagnes", "Muscade", "Gruyère", "Tomatoes", "Red wine", "Beef", "Carrots", "Oignons jaunes", "Parmesan", "Milk", "Basilic", "Céleri", "Ail", "Laurier", "Thym"],
+        picture: "assets/resources/images/Lasagne.jpg",
+        vegetarian: false,
+        price: 9.78,
+    },
+
+    {
+        name: "Lasagne végétarienne",
+        ingredients: ["Oignons", "Gruyère", "Lasagnes", "Courgettes", "Tomatoes", "Basilic", "Herbes de provence", "Fresh cream", "Céleri", "Ail", "Thym"],
+        picture: "assets/resources/images/Lasagne_vegetarienne.jpg",
+        vegetarian: true,
+        price: 11.45,
+    },
+
+    {
+        name: "Poivrons farcis à la grecque",
+        ingredients: ["Tomatoes", "Rice", "Feta", "Oignons", "Pignons de pin", "Dried raisins", "Olive oil", "Curcuma", "Thym"],
+        picture: "assets/resources/images/Poivrons_farcis_grecs.jpg",
+        vegetarian: true,
+        price: 9.98,
+    },
+
+    {
+        name: "Mijoté de tofu aux champignons",
+        ingredients: ["Tofu", "Mushrooms", "Olive oil", "Haricots", "Peas", "Rice", "Carrots"],
+        picture: "assets/resources/images/Tofu_aux_champignons.jpg",
+        vegetarian: true,
+        price: 12.56,
+    },
+
+    {
+        name: "Pâtes carbonara",
+        ingredients: ["Spaghettis", "Eggs", "Parmesan", "Lardons", "Olive oil", "Fresh cream"],
+        picture: "assets/resources/images/Pates_carbonara.jpg",
+        vegetarian: false,
+        price: 8.57,
+    },
+
+    {
+        name: "Tartiflette",
+        ingredients: ["Olive oil", "Reblochon", "Ail", "Lardons fumés", "Potatoes", "Oignons", "Persil"],
+        picture: "assets/resources/images/Tartiflette.jpg",
+        vegetarian: false,
+        price: 10.87,
+    },
+
+    {
+        name: "Boulettes sauce tomate",
+        ingredients: ["Tomatoes", "Beef", "Persil", "Potatoes", "Oignons", "Olive oil", "Ail"],
+        picture: "assets/resources/images/Boulettes_sauce_tomate.jpg",
+        vegetarian: false,
+        price: 8.23,
+    },
+];
+
 
 
 function divise_body() {
@@ -284,17 +286,17 @@ function divise_body() {
 
     let footer = document.createElement("footer");
     document.body.appendChild(footer);
-   
-    return {footer,main,header}
+
+    return { footer, main, header }
 }
 divise_body()
 
 function create_cards(list, type) {
 
-        let category = document.createElement("h1");
-        let categoryContent = document.createTextNode(type);
-        category.appendChild(categoryContent);
-        document.querySelector('main').appendChild(category);
+    let category = document.createElement("h1");
+    let categoryContent = document.createTextNode(type);
+    category.appendChild(categoryContent);
+    document.querySelector('main').appendChild(category);
 
     list.forEach((food, index) => {
 
@@ -312,7 +314,7 @@ function create_cards(list, type) {
         foodImg.alt = "Image of " + food.name;
         foodImg.title = "Description: bla bla bla";
 
-        foodPrice.textContent = "Price " + food.price + " €" ;
+        foodPrice.textContent = "Price " + food.price + " €";
         foodName.textContent = food.name;
         foodIngredients.textContent = "Ingredients: " + food.ingredients.join(", ");
 
@@ -332,3 +334,67 @@ function create_cards(list, type) {
 create_cards(food_entrees, "appetizers");
 create_cards(food_mainCourse, "main course");
 create_cards(food_desserts, "desserts");
+
+
+// Créer un conteneur pour le panier et l'ajouter au corps du document
+const panierContainer = document.createElement('div');
+panierContainer.id = 'panier-container';
+document.body.appendChild(panierContainer);
+
+// Créer une liste non ordonnée pour les articles dans le panier et l'ajouter au conteneur
+const panierListe = document.createElement('ul');
+panierListe.id = 'panier';
+panierContainer.appendChild(panierListe);
+
+// Créer un élément de paragraphe pour afficher le coût total et l'ajouter au conteneur
+const totalElement = document.createElement('p');
+totalElement.id = 'total';
+totalElement.textContent = 'Total : €0.00';
+panierContainer.appendChild(totalElement);
+
+// Tableau pour stocker les articles du panier
+const panierItems = [];
+
+// Fonction pour ajouter un plat au panier
+function ajouterAuPanier(plat) {
+    panierItems.push(plat);
+    afficherPanier();
+}
+
+// Fonction pour afficher les articles dans le panier et mettre à jour le coût total
+function afficherPanier() {
+    panierListe.innerHTML = '';
+
+    let total = 0;
+
+    // Parcourir les articles dans le panier
+    // Créer un élément de liste pour chaque article
+    panierItems.forEach((food) => {
+        const listItem = document.createElement('li');
+        listItem.innerText = `${food.name} - Prix : €${food.price.toFixed(2)}`;
+        panierListe.appendChild(listItem);
+
+        // Ajouter le prix de l'article au total
+        total += food.price;
+    });
+
+
+    // Mettre à jour l'affichage du coût total
+    totalElement.textContent = `Total : €${total.toFixed(2)}`;
+}
+
+// Attendre que le DOM soit entièrement chargé
+document.addEventListener('DOMContentLoaded', function () {
+    // Ajouter un gestionnaire d'événements à chaque bouton 'foodPrice'
+    document.querySelectorAll('.foodPrice').forEach((bouton) => {
+        bouton.addEventListener('click', () => {
+            // Récupérer le nom et le prix du plat à partir des attributs de données
+            const foodName = bouton.dataset.nom;
+            const foodPrice = parseFloat(bouton.dataset.prix);
+
+            // Créer un objet représentant le plat et l'ajouter au panier
+            ajouterAuPanier({ name: foodName, price: foodPrice });
+        });
+    });
+});
+console.log(total)
